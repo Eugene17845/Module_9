1f0d3f5bf52ff4be73bbcf66dc3becd20a08e126
Eugene17845 <razdolbai55h@gmail.com>
2024-11-17 17:44:06 +0300

    Module_9_3.py

--- Module_9_3.py
+++ Module_9_3.py
@@ -2,7 +2,7 @@ first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
 first_result = ((len(x) - len(y)) for x,y in zip(first, second) if len(x) != len(y))
-second_result = (True if len(x)==len(y) else False for x in first for y in second)
+second_result = (True if len(first[x])==len(second[x]) else False for x in range(len(first)))
 
 
 
