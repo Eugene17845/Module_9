22e581cbaf01ac113d4d084bd756898466a38ba6
Eugene17845 <razdolbai55h@gmail.com>
2024-11-21 20:43:33 +0300

    Add files via upload

--- Module_9_5.py
+++ Module_9_5.py
@@ -1,48 +1,10 @@
-class StepValueError(ValueError):
-    pass
-
-class Iterator:
-    def __init__(self, start, stop, step=1):
-        self.start = start
-        self.stop = stop
-        self.step = step
-        if self.step == 0:
-            raise StepValueError('Шаг не может быть равен 0')
-
-    def __iter__(self):
-        self.pointer = self.start
-        return self
-
-    def __next__(self):
-        if (self.step > 0 and self.pointer > self.stop) or (self.step < 0 and self.pointer < self.stop):
-            raise StopIteration
-        result = self.pointer
-        self.pointer += self.step
-        return result
+def all_variants(text):
+    value = len(text)
+    for j in range(value):
+        for g in range(j + 1, value + 1):
+            yield text[j:g]
 
 
-try:
-    iter1 = Iterator(100, 200, 0)
-    for i in iter1:
-        print(i, end=' ')
-except StepValueError:
-    print('Шаг указан неверно')
-
-iter2 = Iterator(-5, 1)
-iter3 = Iterator(6, 15, 2)
-iter4 = Iterator(5, 1, -1)
-iter5 = Iterator(10, 1)
-
-
-for i in iter2:
-    print(i, end=' ')
-print()
-for i in iter3:
-    print(i, end=' ')
-print()
-for i in iter4:
-    print(i, end=' ')
-print()
-for i in iter5:
-    print(i, end=' ')
-print()
+a = all_variants("abc")
+for i in a:
+    print(i)
